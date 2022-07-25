75138be5f4baeb5e092c260c3e447bf119e53a70
MysteryMage <MysteryMage@users.noreply.github.com>
2022-07-25 14:02:35 +0300

    migrated to init.lua from init.vim

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -4,18 +4,18 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 lspconfig.html.setup {
-  capabilities = capabilities,
+    capabilities = capabilities,
 }
 lspconfig.cssls.setup {
-  capabilities = capabilities,
+    capabilities = capabilities,
 }
 
 -- tsserver requires tsconfig.json file, it can be empty
-lspconfig.tsserver.setup{}
+lspconfig.tsserver.setup {}
 
-lspconfig.pyright.setup{}
-lspconfig.vimls.setup{}
-lspconfig.clangd.setup{}
-lspconfig.rust_analyzer.setup{}
-lspconfig.jdtls.setup{}
-lspconfig.sumneko_lua.setup{}
+lspconfig.pyright.setup {}
+lspconfig.vimls.setup {}
+lspconfig.clangd.setup {}
+lspconfig.rust_analyzer.setup {}
+lspconfig.jdtls.setup {}
+lspconfig.sumneko_lua.setup {}
--- nvim/modules/set-vars.vim
+++
@@ -1,9 +0,0 @@
-let g:python3_host_prog = '/Library/Frameworks/Python.framework/Versions/3.9/bin/python3'
-
-let g:UltiSnipsExpandTrigger="<c-v>"
-let g:UltiSnipsJumpForwardTrigger="<c-b>"
-let g:UltiSnipsJumpBackwardTrigger="<c-z>"
-
-let g:netrw_liststyle = 3
-let g:netrw_banner = 0
-let g:netrw_winsize = 25
