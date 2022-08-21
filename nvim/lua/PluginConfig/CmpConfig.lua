06ea74fdc7a7102d0a380a99e1a82eacaf1343d1
MysteryMage <36642316+MysteryMage@users.noreply.github.com>
2022-08-21 17:49:22 +0300

    some fixes

--- nvim/lua/PluginConfig/CmpConfig.lua
+++ nvim/lua/PluginConfig/CmpConfig.lua
@@ -1,31 +1,34 @@
 local cmp = require('cmp')
 local lspkind = require('lspkind')
 
-if cmp ~= nil then
-    cmp.setup {
-        mapping = {
-            ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-            ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        },
-        sources = {
-            { name = 'path' },
-            { name = 'nvim_lua' },
-            { name = 'nvim_lsp' },
-            { name = 'buffer' },
-        },
-        formatting = {
-            format = lspkind.cmp_format({
-                with_text = true,
-                menu = {
-                    buffer = '[buf]',
-                    nvim_lsp = '[LSP]',
-                    nvim_lua = '[api]',
-                    path = '[path]',
-                },
-            })
-        }
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end
+    },
+    mapping = {
+        ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    },
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lua' },
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+    },
+    formatting = {
+        format = lspkind.cmp_format({
+            with_text = true,
+            menu = {
+                buffer = '[buf]',
+                nvim_lsp = '[LSP]',
+                nvim_lua = '[api]',
+                path = '[path]',
+            },
+        })
     }
-end
+}
