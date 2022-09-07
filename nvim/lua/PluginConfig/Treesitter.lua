057dc0be79ad6e9158ea74b05b3f2cf776af5218
MysteryMage <36642316+MysteryMage@users.noreply.github.com>
2022-09-07 18:58:51 +0300

    minor formatting changes

--- nvim/lua/PluginConfig/Treesitter.lua
+++ nvim/lua/PluginConfig/Treesitter.lua
@@ -1,14 +1,15 @@
 require 'nvim-treesitter.install'.compilers = { 'clang' }
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {'c', 'cpp', 'python', 'java', 'javascript', 'vim', 'lua', 'rust', 'go', 'html', 'css', 'norg'},
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { 'c', 'cpp', 'python', 'java', 'javascript', 'vim', 'lua', 'rust', 'go', 'html', 'css',
+        'markdown' },
 
-  sync_install = false,
+    sync_install = false,
 
-  ignore_install = {  },
+    ignore_install = {},
 
-  highlight = {
-    enable = true,
-    disable = {  },
-    additional_vim_regex_highlighting = false,
-  },
+    highlight = {
+        enable = true,
+        disable = {},
+        additional_vim_regex_highlighting = false,
+    },
 }
