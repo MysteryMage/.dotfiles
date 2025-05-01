b64c205fd0279d2b531d975197822caa502b961f
MysteryMage <36642316+MysteryMage@users.noreply.github.com>
2025-05-01 18:17:20 +0300

    Switch to auto lsp config with mason-lspconfig

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -4,63 +4,6 @@ return {
         event = { 'BufReadPre', 'BufNewFile' },
 
         config = function()
-            local lspconfig_ok, lspconfig = pcall(require, 'lspconfig')
-            if not lspconfig_ok then
-                vim.api.nvim_echo({ { '[Plugin] Lspconfig doesn\'t exist' } }, true, { err = false })
-                return
-            end
-
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
-            lspconfig.cssls.setup({
-                capabilities = capabilities,
-            })
-
-            lspconfig.ts_ls.setup({
-                on_attach = function(client)
-                    client.server_capabilities.documentFormattingProvider = false
-                    client.server_capabilities.documentRangeFormattingProvider = false
-                end,
-            })
-            lspconfig.volar.setup({})
-
-            lspconfig.pylsp.setup({})
-            lspconfig.vimls.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.rust_analyzer.setup({})
-            lspconfig.zls.setup({})
-            lspconfig.svelte.setup({})
-
-            lspconfig.lua_ls.setup({
-                settings = {
-                    Lua = {
-                        runtime = {
-                            version = 'LuaJIT',
-                        },
-                        diagnostics = {
-                            enable = true,
-                            globals = { 'vim' },
-                        },
-                        workspace = {
-                            library = {
-                                [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                                [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                                ['/usr/share/awesome/lib'] = true,
-                            },
-                            maxPreload = 2000,
-                            preloadFileSize = 1000,
-                        },
-                        telemetry = {
-                            enable = false,
-                        },
-                    },
-                },
-            })
-
             vim.diagnostic.config({
                 virtual_text = true,
                 update_in_insert = true,
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -16,12 +16,61 @@ return {
                 return
             end
 
+            local lspconfig_ok, lspconfig = pcall(require, 'lspconfig')
+            if not lspconfig_ok then
+                vim.api.nvim_echo({ { '[Plugin] Lspconfig doesn\'t exist' } }, true, { err = false })
+                return
+            end
+
             mason.setup({
                 ui = {
                     border = 'rounded',
                 },
             })
             mason_lsp.setup()
+
+            mason_lsp.setup_handlers({
+                function(server_name)
+                    lspconfig[server_name].setup({})
+                end,
+
+                ['lua_ls'] = function()
+                    lspconfig.lua_ls.setup({
+                        settings = {
+                            Lua = {
+                                runtime = {
+                                    version = 'LuaJIT',
+                                },
+                                diagnostics = {
+                                    enable = true,
+                                    globals = { 'vim' },
+                                },
+                                workspace = {
+                                    library = {
+                                        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                                        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                                        ['/usr/share/awesome/lib'] = true,
+                                    },
+                                    maxPreload = 2000,
+                                    preloadFileSize = 1000,
+                                },
+                                telemetry = {
+                                    enable = false,
+                                },
+                            },
+                        },
+                    })
+                end,
+
+                ['ts_ls'] = function()
+                    lspconfig.ts_ls.setup({
+                        on_attach = function(client)
+                            client.server_capabilities.documentFormattingProvider = false
+                            client.server_capabilities.documentRangeFormattingProvider = false
+                        end,
+                    })
+                end,
+            })
         end,
     },
 }
